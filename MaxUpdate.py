2115e8e6e7f52afe8eaf175c5e107489228530b5
Rodrigo Nunes <rrgnunes@gmail.com>
2024-01-23 09:36:14 -0300

    modified:   MaxUpdate.py

--- MaxUpdate.py
+++ MaxUpdate.py
@@ -41,13 +41,14 @@ class MaxUpdate(win32serviceutil.ServiceFramework):
             try:
                 print('Bem... Vamos lá')
 
-                producao = -1
-                while producao == -1:
-                    path_config_thread = SCRIPT_PATH + "/config.json"
-                    if os.path.exists(path_config_thread):
-                        with open(path_config_thread, 'r') as config_file:
-                            config_thread = json.load(config_file)
-                    producao = config_thread['producao']
+                producao = 0
+            
+                path_config_thread = SCRIPT_PATH + "/config.json"
+                if os.path.exists(path_config_thread):
+                    with open(path_config_thread, 'r') as config_file:
+                        config_thread = json.load(config_file)
+                producao = config_thread['producao']
+                
                 if producao == 0:
                     SCRIPT_URL = 'http://maxsuport.com.br:81/static/hom_update/MaxUpdate.py'
 
