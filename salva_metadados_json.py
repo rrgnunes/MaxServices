5e7718abaab9699229901ca0ab9e8eb39f6344cd
Everton-Rian <rianmailteste@gmail.com>
2025-04-09 15:07:22 -0400

    inicio de midificacao thread atualiza banco

--- salva_metadados_json.py
+++ salva_metadados_json.py
@@ -2,7 +2,7 @@ import json
 import parametros
 from funcoes import *
 
-def salvar_json():
+def salvar_json_metadados_master():
     pasta_metadados = os.path.join('/home', 'banco_json')
     if not os.path.exists(pasta_metadados):
         os.makedirs(pasta_metadados)
@@ -66,6 +66,70 @@ def salvar_json():
     except Exception as e:
         print_log(f'Erro ao salvar JSONs -> motivo: {e.__class__.__name__}-{e}')
 
+def salva_json_metadados_local(caminho_base_dados:str):
+    try:
+        carrega_arquivo_config()
+
+        parametros.DATABASEFB = caminho_base_dados
+        client_dll = verifica_dll_firebird()
+
+        inicializa_conexao_firebird(client_dll)
+    except Exception as e:
+        print_log(f'Nao foi possivel conectar ao banco de dados -> motivo: {e}', 'salva_metadados_json')
+
+    try:
+        pasta_metadados = os.path.join(parametros.SCRIPT_PATH, 'metadados_local')
+        if not os.path.exists(pasta_metadados):
+            os.makedirs(pasta_metadados)
+
+        with parametros.FIREBIRD_CONNECTION as con:
+
+            metadados_tabelas = extrair_metadados_tabelas_firebird(con)
+            if metadados_tabelas:
+                with open(os.path.join(pasta_metadados,"estrutura_banco.json"), 'w') as j:
+                    json.dump(metadados_tabelas, j, indent=4)
+                print_log('Arquivo de estrutura do banco salvo com sucesso', 'salva_metadados_json')
+            else:
+                print_log('Falha ao salvar estrutura de banco', 'salva_metadados_json')
+
+            # salva as chaves primarias das tabelas
+            metadados_chaves = extrair_metadados_chaves_primarias_firebird(con)
+            if metadados_chaves:
+                with open(os.path.join(pasta_metadados,"chaves_primarias.json"), 'w') as j:
+                    json.dump(metadados_chaves, j, indent=2)
+                print_log('Arquivo de chaves primarias salvo com sucesso', 'salva_metadados_json')
+            else:
+                print_log('Falha ao salvar as chaves primarias.', 'salva_metadados_json')
+
+            # salva as chaves estrangeiras das tabelas
+            metadados_chaves_estrangeiras = extrair_metadados_chaves_estrangeiras(con)
+            if metadados_chaves_estrangeiras:
+                with open(os.path.join(pasta_metadados,"chaves_estrangeiras.json"), 'w') as j:
+                    json.dump(metadados_chaves_estrangeiras, j, indent=2)
+                print_log('Arquivo de chaves estrangeiras salvo com sucesso', 'salva_metadados_json')
+            else:
+                print_log('Falha ao salvar as chaves estrangeiras.', 'salva_metadados_json')
+
+            # salva o sql das procedures
+            metadados_procedures = extrair_metadados_procedures(con)
+            if metadados_procedures:
+                with open(os.path.join(pasta_metadados,"procedures.json"), 'w') as j:
+                    json.dump(metadados_procedures, j, indent=2)
+                print_log('Arquivo de procedures salvo com sucesso', 'salva_metadados_json')
+            else:
+                print_log('Falha ao salvar sql das procedures.', 'salva_metadados_json')
+            
+            # salva o sql das triggers
+            metadados_triggers = extrair_metadados_triggers(con)
+            if metadados_triggers:
+                with open(os.path.join(pasta_metadados,'triggers.json'), 'w') as j:
+                    json.dump(metadados_triggers, j, indent=2)
+                print_log('Arquivo de tiggers salvo com sucesso', 'salva_metadados_json')
+            else:
+                print_log('Falha ao salvar sql das triggers.', 'salva_metadados_json')
+    except Exception as e:
+        print_log(f'Erro ao salvar arquivo JSON da estrutura banco local -> motivo: {e}')
+
 if __name__ == '__main__':
 
-    salvar_json()
+    salva_json_metadados_local()
--- thread_atualiza_banco.py
+++ thread_atualiza_banco.py
@@ -1,15 +1,18 @@
 import sys
 import fdb
-from funcoes import os, extrair_metadados, gerar_scripts_diferencas, executar_scripts_sql, print_log, criar_bloqueio, remover_bloqueio, pode_executar, carrega_arquivo_config
 import parametros
 import configparser
 import os
+from salva_metadados_json import salva_json_metadados_local
+from funcoes import os, extrair_metadados, gerar_scripts_diferencas, executar_scripts_sql, print_log, criar_bloqueio, remover_bloqueio, pode_executar, carrega_arquivo_config
+
 
 def atualiza_banco():
     nome_servico = 'thread_atualiza_banco'
+    pasta_metadados_local = os.path.join(parametros.SCRIPT_PATH, 'metadados_local')
+    pasta_metadados_remoto = os.path.join(parametros.SCRIPT_PATH, 'metadados_remoto')
     carrega_arquivo_config()
     try:
-
         for cnpj, dados_cnpj in parametros.CNPJ_CONFIG['sistema'].items():
             ativo = dados_cnpj['sistema_ativo'] == '1'
             sistema_em_uso = dados_cnpj['sistema_em_uso_id']
@@ -17,75 +20,38 @@ def atualiza_banco():
             porta_firebird_maxsuport = dados_cnpj['porta_firebird_maxsuport']
             caminho_gbak_firebird_maxsuport = dados_cnpj['caminho_gbak_firebird_maxsuport']
 
+            # Pula para a proxima configuracao caso nao tenha o caminho do banco de dados
             if caminho_base_dados_maxsuport == 'None':
                 continue
 
             caminho_sistema = caminho_base_dados_maxsuport.lower().replace('\\dados\\dados.fdb', '')
             caminho_ini = os.path.join(caminho_sistema, 'banco.ini')
-            # Inicializa o parser
-            config = configparser.ConfigParser()
+        
 
-            # Carrega o arquivo INI
+            config = configparser.ConfigParser()
             config.read(caminho_ini)
 
             if ativo and sistema_em_uso == '1':
-                print_log(f'Vou checar se é para atualizar -> Pasta: {caminho_base_dados_maxsuport} - CNPJ: {cnpj}', nome_servico)
+                print_log(f'Verificando se pode ser atualizado -> {caminho_base_dados_maxsuport} CNPJ:{cnpj}', nome_servico)
                 atualiza_banco = 0
 
-                if 'manutencao' in config and 'atualizabanco' in config['manutencao']:
+                if ('manutencao' in config) and ('atualizabanco' in config['manutencao']):
                     atualiza_banco = config['manutencao']['atualizabanco']
 
                 if str(atualiza_banco) == '1':
-                    server_origem = "maxsuportsistemas.com"
-                    port_origem = 3050
-                    path_origem = "/home/base/dados.fdb"
 
-                    server_destino = "127.0.0.1"
-                    port_destino = porta_firebird_maxsuport
-                    path_destino = caminho_base_dados_maxsuport
-                    user_destino = 'maxsuport'
-                    password_destino = parametros.PASSFB
+                    print_log('Salvando estrutura do banco de dados local...', nome_servico)
+                    salva_json_metadados_local(caminho_base_dados_maxsuport)
 
-                    dsn_origem = f"{server_origem}/{port_origem}:{path_origem}"
-                    dsn_destino = f"{server_destino}/{port_destino}:{path_destino}"
+                    if (os.path.exists(pasta_metadados_local)) and (os.path.exists(pasta_metadados_remoto)):
+                        ...
+                    else:
+                        print_log('Pasta metadados nao existe!', nome_servico)
 
-                    fdb.load_api(f'{caminho_gbak_firebird_maxsuport}/fbclient.dll')
 
-                    try:
-                        conexao_origem = fdb.connect(dsn=dsn_origem, user='sysdba', password='masterkey', charset='UTF-8')
-                        conexao_destino = fdb.connect(dsn=dsn_destino, user=user_destino, password=password_destino, charset='UTF-8')
 
-                        metadados_origem = extrair_metadados(conexao_origem)
-                        metadados_destino = extrair_metadados(conexao_destino)
-                        
-                        scripts_sql = gerar_scripts_diferencas(metadados_origem, metadados_destino)
-
-                        erros = executar_scripts_sql(conexao_destino, scripts_sql, nome_servico)
-
-                        if erros:
-                            print_log("Erros encontrados durante a execução dos scripts:", nome_servico)
-                            for erro in erros:
-                                print_log(f"    Script: {erro['script']}\nErro: {erro['erro']}\n", nome_servico)
-                        else:
-                            print_log('Todos os scripts foram executados com sucesso.', nome_servico)
-
-                        config['manutencao']['atualizabanco'] = '0'
-
-                        # Salva as mudanças de volta no arquivo INI
-                        with open(caminho_ini, 'w') as configfile:
-                            config.write(configfile)
-
-                        print_log('Tabela atualizada', nome_servico)
-                    finally:
-                        if not conexao_origem.closed:
-                            conexao_origem.close()
-                        
-                        if not conexao_destino.closed:
-                            conexao_destino.close()
-                            
     except Exception as e:
-        print_log(f"Erro na atualização do banco: {e}", nome_servico)
-
+        print_log(f'Nao foi possivel atualizar banco de dados -> motivo: {e}', nome_servico)
 
 if __name__ == '__main__':
 
